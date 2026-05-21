facc403928d3122f47f56158ab38eb34d7c36bfd
Henry <henry.abramovich@gmail.com>
2026-05-21 02:20:25 -0400

    fix: use native vitest ts config option

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
@@ -15,6 +14,7 @@ export default defineConfig({
     }
   },
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@": path.resolve(__dirname, "./")
     }
