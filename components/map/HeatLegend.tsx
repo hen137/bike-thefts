546efbdc363255a446aa363c9700402b6851993d
Henry <henry.abramovich@gmail.com>
2026-06-07 23:43:33 -0400

    feat: quanitfy heatmap with lock icons

--- components/map/HeatLegend.tsx
+++ components/map/HeatLegend.tsx
@@ -1,3 +1,4 @@
+import Lock from "@/assets/lock.svg";
 // const LEGEND_DIVISION = 3;
 
 export function HeatLegend() {
@@ -5,8 +6,15 @@ export function HeatLegend() {
     <div className="h-10 w-100 pl-3 pr-3  flex flex-col justify-center bg-white dark:bg-slate-700 shadow-lg">
       {/* Scale */}
       <div className=" h-5/10 w-full items-center flex flex-row justify-between ml-0.5 mr-0.5 text-xs pl-2 pr-2 bg-linear-to-r from-blue-600 via-green-400 to-red-500">
-        <p>Less</p>
-        <p>More</p>
+        {/* <p>Less</p> */}
+        <Lock className="size-3 rotate-225 text-white translate-y-px" />
+
+        {/* <p>More</p> */}
+        <div className="flex translate-y-px">
+          <Lock className="size-3 rotate-225 text-white" />
+          <Lock className="size-3 rotate-225 text-white" />
+          <Lock className="size-3 rotate-225 text-white" />
+        </div>
       </div>
     </div>
   );
