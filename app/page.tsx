72649cdb2a83f5ed8f953e7a077d79aba09c4f43
Henry Abramovich <henry.abramovich@gmail.com>
2026-06-16 00:03:30 -0400

    fix: scale map to page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NavBar } from "@/components/ui/NavBar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex flex-col gap-4 sm:h-185 sm:w-220 sm:absolute sm:bottom-5 sm:left-1/2 sm:-translate-x-1/2 lg:h-248 lg:w-450">
+    <div className="h-screen w-full flex flex-col gap-4 sm:h-95/100 sm:w-95/100 sm:absolute sm:bottom-5 sm:left-1/2 sm:-translate-x-1/2">
       <MapErrorBoundary>
         <MapProvider>
           <TileProvider>
